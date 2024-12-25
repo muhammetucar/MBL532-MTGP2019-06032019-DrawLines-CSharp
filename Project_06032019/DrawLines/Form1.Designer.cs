a57476460aab5d29a1ffd846114710346671467c
muhammetucar <muhammetucarr@gmail.com>
2024-12-25 22:17:20 +0300

    MBL532-MTGP2019-DrawLines VS Files

---
+++ Project_06032019/DrawLines/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace DrawLines
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Angle = new System.Windows.Forms.TextBox();
+            this.Distance = new System.Windows.Forms.TextBox();
+            this.Length = new System.Windows.Forms.TextBox();
+            this.NumberofLines = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.Angle);
+            this.panel1.Controls.Add(this.Distance);
+            this.panel1.Controls.Add(this.Length);
+            this.panel1.Controls.Add(this.NumberofLines);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1468, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(482, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "PARTY STARTER";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Angle
+            // 
+            this.Angle.Location = new System.Drawing.Point(365, 25);
+            this.Angle.Name = "Angle";
+            this.Angle.Size = new System.Drawing.Size(100, 20);
+            this.Angle.TabIndex = 7;
+            this.Angle.Text = "15";
+            // 
+            // Distance
+            // 
+            this.Distance.Location = new System.Drawing.Point(240, 25);
+            this.Distance.Name = "Distance";
+            this.Distance.Size = new System.Drawing.Size(100, 20);
+            this.Distance.TabIndex = 6;
+            this.Distance.Text = "25";
+            // 
+            // Length
+            // 
+            this.Length.Location = new System.Drawing.Point(124, 25);
+            this.Length.Name = "Length";
+            this.Length.Size = new System.Drawing.Size(100, 20);
+            this.Length.TabIndex = 5;
+            this.Length.Text = "50";
+            // 
+            // NumberofLines
+            // 
+            this.NumberofLines.Location = new System.Drawing.Point(12, 25);
+            this.NumberofLines.Name = "NumberofLines";
+            this.NumberofLines.Size = new System.Drawing.Size(100, 20);
+            this.NumberofLines.TabIndex = 4;
+            this.NumberofLines.Text = "100";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(362, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Angle";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(236, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Distance";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(121, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Length";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Number of Lines";
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 106);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1468, 520);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1468, 626);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Angle;
+        private System.Windows.Forms.TextBox Distance;
+        private System.Windows.Forms.TextBox Length;
+        private System.Windows.Forms.TextBox NumberofLines;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Project_06032019/DrawLines/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawLines
+{
+    public partial class Form1 : Form
+    {
+        Pen myPen = new Pen(Color.Black);
+        Graphics g = null;
+        static int startX, startY;
+        static int endX, endY;
+        static int myAngle = 0;
+        static int myLength = 0;
+        static int myDistance = 0;
+        static int numberLines = 0;
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //convert text to integer
+            myAngle = int.Parse(Angle.Text);
+            myLength = int.Parse(Length.Text);
+            myDistance = int.Parse(Distance.Text);
+            numberLines = int.Parse(NumberofLines.Text);
+
+            //locate patterns in the center of the form
+            startX = panel2.Width / 2;
+            startY = panel2.Height / 2;
+
+            //refresh canvas
+            panel2.Refresh();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            myPen.Width = 1; //pen width
+            g = panel2.CreateGraphics();
+            for (int i = 0; i < int.Parse(NumberofLines.Text); i++)
+                drawLine();
+        }
+
+        private void drawLine()
+        {
+            //choose random pen color
+            Random randomGen = new Random();
+            myPen.Color = Color.FromArgb(randomGen.Next(255), randomGen.Next(255), randomGen.Next(255));
+
+            myAngle = myAngle + int.Parse(Angle.Text); //add line to existing line
+            myLength = myLength + int.Parse(Distance.Text); //add length to existing length
+
+            //draw radiant lines:
+            endX = (int)(startX + Math.Cos(Math.PI * myAngle / 180) * myLength);
+            endY = (int)(startY + Math.Sin(Math.PI * myAngle / 180) * myLength);
+            Point[] points = { new Point(startX, startY), new Point(endX, endY) };
+            //end of a line is the starting point of the next line:
+            startX = endX;
+            startY = endY;
+            g.DrawLines(myPen, points);
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
